2bb0e786da40af64fc4747cf8f1fb86fb29eff74
Michele Mottini <michele.mottini@gmail.com>
2015-02-09 09:56:22 -0500

    PDemo: created

---
+++ PDemo/Readme.md
@@ -0,0 +1,69 @@
+# PDemo
+
+*[FHIR](http://www.hl7.org/implement/standards/fhir/)-based Patients' DEMOgraphics*
+
+Simple HTML+JavaScript single page application that query patients' demographics information from a FHIR server.
+
+It is a consumer implementation of the [IHE PDQm profile](http://www.ihe.net/uploadedFiles/Documents/ITI/IHE_ITI_Suppl_PDQm_Rev1.0_PC_2014-06-06.pdf).
+
+## Basic usage  - no authentication
+
+To start PDemo navigate with your browser to `PDemo.html?fhirServiceUrl=xxxxx`, where xxxx is the URL of the FHIR server to use. For example:
+
+`https://demo.careevolution.com/PDemo/PDemo.html?fhirServiceUrl=https://fhir-open-api.smartplatforms.org`
+
+starts PDemo connecting to the [SMART on FHIR](http://docs.smartplatforms.org/) demo anonymous server, and:
+
+`https://demo.careevolution.com/PDemo/PDemo.html?fhirServiceUrl=http://spark.furore.com/fhir`
+
+starts PDemo connecting to the [Furore](http://spark.furore.com/) demo server. 
+
+When starting in this way the app does not use authentication, and so the FHIR server must accept anonymous (non authenticated) requests.
+
+If `PDemo.html` is served using HTTPS and the FHIR server uses non-secure HTTP (as in the second example above) the browser will complain about 'mixed content' or 'unauthenticated sources' and it will be necessary to confirm that that is OK for the app to work:
+
+![Unauthenticated sources in Chrome](Documentation/Chrome_unauthorized.png)
+
+In general `PDemo.html` will be hosted on a server different from the FHIR one, hence the FHIR server must support [CORS](http://en.wikipedia.org/wiki/Cross-origin_resource_sharing).
+
+## Using SMART authentication
+
+[SMART on FHIR](http://docs.smartplatforms.org/) is an extension of the FHIR API that defines an authentication protocol based on [OAuth2](http://oauth.net/2/).
+
+To use SMART authentication specify the URL of the FHIR server using `iss` instead than `fhirServiceUrl`. For example:
+
+`https://demo.careevolution.com/PDemo/PDemo.html?iss=https://fhir-api.smartplatforms.org`
+
+starts PDemo connecting to the [SMART on FHIR](http://docs.smartplatforms.org/) demo authenticated server. 
+
+When using the `iss` parameter PDemo follows the [SMART authorization sequence](http://docs.smartplatforms.org/authorization/public/), redirecting the browser to the login page associated with the FHIR server:
+
+![SMART login](Documentation/SMART_login.png)
+
+Once the user logs in and authorizes the app the browser redirects back to PDemo, that uses the user credentials to access the FHIR server.
+
+PDemo is registered with the [authorization server](https://authorize.smartplatforms.org/) used by the SMART demo server. The registration includes the list of authorized startup URLs, so PDemo can be run only as `https://demo.careevolution.com/PDemo/PDemo.html` when accessing the SMART demo server. To run PDemo from another server it is necessary to create a new registration - and change its client ID to match the new registration. The client ID is in `PDemoConfiguration.js`
+
+## Using login authentication
+
+PDemo can login directly to a FHIR server that supports [OAuth2 password credentials grant](http://tools.ietf.org/html/rfc6749#section-4.3).
+
+To use login authentication specify the URL of the FHIR server as in the 'no authentication case' and specify the OAuth2 password credential grant URL using the `tokenUrl` parameter:
+
+`https://demo.careevolution.com/PDemo/PDemo.html?fhirServiceUrl=https://careev13:8080/fhir&tokenUrl=https://careev13:8080/OAuth2/Token`
+
+PDemo displays an additional login form in the top right corner:
+
+![PDemo login](Documentation/PDemo_login.png)
+
+Entering user name and password and cliking on 'Sign in' sends the password credential grant request to the token URL (`https://careev13:8080/OAuth2/Token` in the example), if successful then PDemo uses those user credentials to access the FHIR server:
+
+![PDemo logged-in](Documentation/PDemo_logged_in.png)
+
+## Dependencies
+
+PDemo uses jQuery, AngularJS and Bootstrap - all loaded from CDN locations, so a working Internet connection is needed - even if the app and the FHIR server are local.
+
+## Browsers compatibility
+
+Tested (mostly) with IE 11 and Chrome. It should work on any modern browser.
---
+++ PDemo/Scripts/PDemoConfiguration.js
@@ -0,0 +1,114 @@
+﻿/* 
+ * Copyright (c) 2015, CareEvolution Inc (info@careevolution.com)
+ * 
+ * This file is licensed under the MIT License - see License.txt
+ */
+
+var PDemoConfiguration = {
+	// OAuth2 authorization server endpoint (http://tools.ietf.org/html/rfc6749#section-4.3.2)
+	// Set it to null to disable authorization and use FHIR in anonymous mode
+	oauth2Url: null, // "https://careev13:8080/OAuth2/Token",
+
+	// Client identifier used by the OAuth2 protocol - it must be accepted by the OAuth2 authorization server
+	clientID: "a8076d0c-d6cc-4bfc-bdf9-3115bb469ace",
+
+	// Possible sorts
+	sorts: [
+		{ directive: { field: "name", direction: "asc" }, name: "Full name - ascending" },
+		{ directive: { field: "name", direction: "desc" }, name: "Full name - descending" },
+		{ directive: { field: "family", direction: "asc" }, name: "Last name - ascending" },
+		{ directive: { field: "family", direction: "desc" }, name: "Last name - descending" },
+		{ directive: { field: "given", direction: "asc" }, name: "First name - ascending" },
+		{ directive: { field: "given", direction: "desc" }, name: "First name - descending" },
+		{ directive: { field: "birthdate", direction: "asc" }, name: "Birth date - ascending" },
+		{ directive: { field: "birthdate", direction: "desc" }, name: "Birth date - descending" },
+		{ directive: { field: "address", direction: "asc" }, name: "Address - ascending" },
+		{ directive: { field: "address", direction: "desc" }, name: "Address - descending" },
+		{ directive: { field: "telecom", direction: "asc" }, name: "Contact - ascending" },
+		{ directive: { field: "telecom", direction: "desc" }, name: "Contact - descending" },
+	],
+
+	defaultResultsPerPage: 10,
+
+	// Default search fields to use. 
+	defaultSearchFields: {
+		identifier: true,
+		name: true,
+		family: false,
+		given: false,
+		birthdate: true,
+		address: false,
+		gender: true,
+		telecom: false,
+	},
+
+	defaultSearchIdentifierSystems: [
+		{ uri: "urn:oid:2.16.840.1.113883.4.1", name: "SSN" },
+	],
+
+	defaultGenderValueSet: {
+		uri: "urn:oid:2.16.840.1.113883.1.11.1",
+		values: [
+			{ code: "F", name: "Female" },
+			{ code: "M", name: "Male" },
+			{ code: "UN", name: "Undifferentiated" },
+		]
+	},
+
+	// See http://www.hl7.org/implement/standards/fhir/terminologies-systems.html#identifiersystems
+	knownIdentifierSystems: [
+		{ uri: "urn:ietf:rfc:3986", name: "" },	// no display name needed for a URL identifier
+		{ uri: "http://hl7.org/fhir/sid/us-ssn", name: "SSN" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.1", name: "SSN" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.2", name: "Driver's license - AK" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.1", name: "Driver's license - AL" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.5", name: "Driver's license - AR" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.4", name: "Driver's license - AZ" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.6", name: "Driver's license - CA" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.8", name: "Driver's license - CO" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.9", name: "Driver's license - CT" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.11", name: "Driver's license - DC" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.10", name: "Driver's license - DE" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.12", name: "Driver's license - FL" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.13", name: "Driver's license - GA" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.15", name: "Driver's license - HI" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.19", name: "Driver's license - IA" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.16", name: "Driver's license - ID" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.17", name: "Driver's license - IL" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.20", name: "Driver's license - KS" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.21", name: "Driver's license - KY" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.22", name: "Driver's license - LA" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.25", name: "Driver's license - MA" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.24", name: "Driver's license - MD" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.23", name: "Driver's license - ME" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.26", name: "Driver's license - MI" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.27", name: "Driver's license - MN" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.29", name: "Driver's license - MO" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.28", name: "Driver's license - MS" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.30", name: "Driver's license - MT" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.36", name: "Driver's license - MY" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.37", name: "Driver's license - NC" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.38", name: "Driver's license - ND" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.31", name: "Driver's license - NE" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.33", name: "Driver's license - NH" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.34", name: "Driver's license - NJ" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.35", name: "Driver's license - NM" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.32", name: "Driver's license - NV" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.39", name: "Driver's license - OH" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.40", name: "Driver's license - OK" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.41", name: "Driver's license - OR" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.42", name: "Driver's license - PA" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.44", name: "Driver's license - RI" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.45", name: "Driver's license - SC" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.46", name: "Driver's license - SD" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.47", name: "Driver's license - TN" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.48", name: "Driver's license - TX" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.49", name: "Driver's license - UT" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.51", name: "Driver's license - VA" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.50", name: "Driver's license - VT" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.53", name: "Driver's license - WA" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.55", name: "Driver's license - WI" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.54", name: "Driver's license - WV" },
+		{ uri: "urn:oid:2.16.840.1.113883.4.3.56", name: "Driver's license - WY" },
+	],
+};
